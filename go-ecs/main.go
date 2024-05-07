d688d5388350155eb3b717c673b87d814178731d
slide <TheBlakkthorn@gmail.com>
2024-05-06 21:15:49 -0700

    Part 1 implemented

--- go-ecs/main.go
+++ go-ecs/main.go
@@ -3,6 +3,8 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strconv"
 
 	"github.com/klauspost/cpuid"
 )
@@ -19,6 +21,9 @@ type ArchetypeID int
 // ComponentType is a slice of ComponentIDs
 type ComponentType []ComponentID
 
+// ComponentTypeKey is a sorted string of each integer in the slice
+type ComponentTypeKey string
+
 // Archetype is a unique collection of Component Ids that define it
 type Archetype struct {
 	ArchetypeID ArchetypeID
@@ -30,15 +35,16 @@ type ArchetypeSet Set[ArchetypeID]
 
 // World contains the EntityIndex and ComponentIndex
 type World struct {
-	EntityIndex map[EntityID]*Archetype
-	ComponentIndex map[ComponentID]ArchetypeSet
+	EntityIndex    map[EntityID]*Archetype
+	ComponentIndex map[ComponentID]Set[int] // Set[int] = ArchetypeSet
+	ArchetypeIndex map[ComponentTypeKey]Archetype
 }
 
-func (p *World) HasComponent(entityID EntityID, componentID ComponentID) {
+func (p *World) HasComponent(entityID EntityID, componentID ComponentID) bool {
 	archetype := p.EntityIndex[entityID]
 	archetypeSet := p.ComponentIndex[componentID]
 
-	return ((Set)archetypeSet).Contains()
+	return archetypeSet.Contains(int(archetype.ArchetypeID))
 }
 
 func main() {
@@ -54,3 +60,29 @@ func main() {
 	}
 
 }
+
+func sliceKey(ints []int) string {
+	// Sort the integers to ensure the same order produces the same string
+	sort.Ints(ints)
+
+	// Convert each integer to a string and concatenate them
+	var str string
+	for _, num := range ints {
+		str += strconv.Itoa(num) + ","
+	}
+	// Remove the trailing comma
+	str = str[:len(str)-1]
+
+	return str
+}
+
+// // A list of component ids
+// using Type = vector<ComponentId>;// Type used to store each unique component list only once
+// struct Archetype {
+//   ArchetypeId id;
+//   Type type;
+// };// Find an archetype by its list of component ids
+// unordered_map<Type, Archetype> archetype_index;// Find the archetype for an entity
+// unordered_map<EntityId, Archetype&> entity_index;// Find the archetypes for a component
+// using ArchetypeSet = unordered_set<ArchetypeId>;
+// unordered_map<ComponentId, ArchetypeSet> component_index;
